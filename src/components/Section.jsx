87938c2b4fbc31b74e18a8b01b6fbf70f640e1c3
Bidhu1024 <bidhu1024av@gmail.com>
2022-12-08 22:29:15 +0530

    44:42

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,19 +1,73 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-import backg from "../Assets/model-s.jpg"
+import backg from "../Assets/model-s.jpg";
+import downarr from "../Assets/down-arrow.svg";
 const Section = () => {
   return (
     <Wrap>
-
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src={downarr}></DownArrow>
+      </Buttons>
     </Wrap>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
 
 const Wrap = styled.div`
-width: 100vw;
-height: 100vh;
-/* background: orange; */
-background-image: url('../Assets/model-s.jpg');
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url(${backg});
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 2rem;
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 2.5rem;
+  width: 16rem;
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: .5rem;
+
+`;
+const RightButton = styled(LeftButton)`
+background-color: #fff;
+color: black;
+`;
+const DownArrow = styled.img`
+  margin: 1.25rem;
+  height: 2.5rem;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+const Buttons = styled.div`
+
 `;
