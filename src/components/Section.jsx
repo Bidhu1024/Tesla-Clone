066242ee8762afee04e0054957421a1b21788886
Bidhu1024 <bidhu1024av@gmail.com>
2022-12-08 08:37:45 +0530

    21:46

---
+++ src/components/Section.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from "styled-components";
+import backg from "../Assets/model-s.jpg"
+const Section = () => {
+  return (
+    <Wrap>
+
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+width: 100vw;
+height: 100vh;
+/* background: orange; */
+background-image: url('../Assets/model-s.jpg');
+`;
